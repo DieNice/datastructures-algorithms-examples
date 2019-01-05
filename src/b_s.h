6dd1a14cf9651ec6cd4663d11427fcea325aaf12
Denis <DailyPlanning@bk.ru>
2019-01-06 00:49:57 +1000

    Remake permutations

--- src/b_s.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_BINARY_SEARCH
-#define MY_BINARY_SEARCH
-
-int Search_Binary(int arr[], int N, int key);
-
-#endif //Функция бинарного(логарифмического) поиска
--- src/i_b_s.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_INT_BINARY_SEARCH
-#define MY_INT_BINARY_SEARCH
-
-int Int_Search_Binary(int A[], int N, int key);
-
-#endif
